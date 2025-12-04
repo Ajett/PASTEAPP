3522e6167c3db1f48809482df8d7db75f2b15664
Ajeet Kumar <130174199+Ajett@users.noreply.github.com>
2025-12-04 22:19:10 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-const API = "http://localhost:5000/api/auth";
+const API = "https://pasteapp-ovci.onrender.com/api/auth";
 
 export const registerAPI = (data) => axios.post(`${API}/register`, data);
 export const loginAPI = (data) => axios.post(`${API}/login`, data);
